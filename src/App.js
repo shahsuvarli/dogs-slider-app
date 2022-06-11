3053ac66e4e648ffbb06b59d2f00cac6d4ab131c
Elvin Shahsuvarli <shahsuvarli.elvin@gmail.com>
2022-06-11 18:46:35 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import dogs from "./dogs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: dogs,
+      num: 0,
+    };
+    this.decrease = this.decrease.bind(this);
+    this.increase = this.increase.bind(this);
+  }
+  decrease() {
+    if (this.state.num) {
+      this.setState({ num: this.state.num - 1 });
+    } else {
+      this.setState({ num: this.state.data.length - 1 });
+    }
+  }
+  increase() {
+    if (this.state.num === this.state.data.length - 1) {
+      this.setState({ num: 0 });
+    } else {
+      this.setState({ num: this.state.num + 1 });
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="main">
+          <h1>My Dogs</h1>
+          <div className="image-container">
+            <img
+              src={this.state.data[this.state.num].url}
+              alt="dog"
+              className="image"
+            />
+          </div>
+          <div className="menu">
+            <button onClick={this.decrease}>previous</button>
+            <h2>Image: {this.state.num+1}</h2>
+            <button onClick={this.increase}>next</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/dogs.js
@@ -0,0 +1,16 @@
+const dogs = [
+  {
+    id: 0,
+    url: "https://upload.wikimedia.org/wikipedia/commons/1/18/Dog_Breeds.jpg",
+  },
+  {
+    id: 1,
+    url: "https://upload.wikimedia.org/wikipedia/commons/6/62/Big_and_little_dog.jpg",
+  },
+  {
+    id: 2,
+    url: "https://upload.wikimedia.org/wikipedia/commons/a/af/Golden_retriever_eating_pigs_foot.jpg",
+  },
+];
+
+export default dogs;
